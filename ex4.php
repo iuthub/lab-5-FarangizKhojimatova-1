6b74f79d1e3cc3fee5200948332510219a551ee6
FarangizKhojimatova <72074032+FarangizKhojimatova@users.noreply.github.com>
2021-03-24 02:03:59 +0500

    program 4 completed

---
+++ ex4.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Buy Your Way to a Better Education!</title>
+    <link href="buyagrade.css" type="text/css" rel="stylesheet" />
+</head>
+<style>
+    html {
+        background-color: #39275B;
+    }
+
+    body {
+        width: 600px;
+        margin: 1em auto;
+        background-color: white;
+        border: 10px #D8C858 solid;
+        padding: 0.5em;
+    }
+
+    hr {
+        width: 90%;
+        border: 1px solid #D8C858;
+    }
+
+    h1, h2 {
+        font-family: sans-serif;
+        text-align: center;
+    }
+
+    input[type="submit"] {
+        font-size: 2em;
+        width: 100%;
+    }
+</style>
+<form action="http://httpbin.org/post"
+<body>
+<?php
+if(isset($_POST['Name']) && ($_POST['Section']) &&  ($_POST['cardnumber']) && ($_POST['cardtype']))
+{
+    $Name=$_POST['Name'];
+    $Section=$_POST['Section'];
+    $cardnumber=$_POST['cardnumber'];
+$cardtype=$_POST['cardtype'];}
+?>
+
+<h1>Sorry</h1>
+<p>You didn't fill out the form completely.  <a href="ex3.html">Try again?</a></p>
+</body>
+</html>
